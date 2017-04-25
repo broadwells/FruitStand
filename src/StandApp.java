83ffecd224426ae2b0daad0a244fdd4d89a1ef6e
mlriley77 <rileycode77@gmail.com>
2017-04-25 13:07:41 -0400

    mikes commit

--- src/StandApp.java
+++ src/StandApp.java
@@ -3,6 +3,11 @@
  */
 public class StandApp {
     public static void main(String[] args) {
+
+
         System.out.println("Hello World!");
+        System.out.println("Group 1 is awesome");
     }
+
+
 }
