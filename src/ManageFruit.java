e713dca5d3c328fd0993231a2ae620fd618ed915
LydiaLab <lydia@lydialatocki.com>
2017-04-25 23:41:20 -0400

    new class

---
+++ src/ManageFruit.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Pro on 4/25/17.
+ */
+public class ManageFruit {
+    private ArrayList<Fruit> fruitList = new ArrayList<Fruit>();
+
+    public manageFruit(ArrayList<Fruit> fruitList){ // constructor setting fruit into arrayList
+        this.fruitList = fruitList;
+    }
+
+    public void listAllFruit (){ // list complete info: name, sku, category, descript, price
+        for (int i=0; i<fruitList.size(); i++){
+            System.out.println(i + fruitList.get[i].getName() + fruitList.get[i].getSku() + fruitList.get[i].getCategory() + fruitList.getdescript() + fruitList.getPrice());
+        }
+    }
+
+
+    public void findFruitName () { // used for String search - print fruit: name, sku only
+        for (int i=0; i<fruitList.size(); i++){
+            if (i == userInput){ // userInput in search for fruit by name FIXME to print all fruit w/ that name
+                System.out.println(i + ": " + fruitList.get[i].getName() + fruitList.get[i].getSku()); //- print fruit: name, sku only
+            }
+        }
+    }
+
+
+    public void findFruitSku () { // used for int search - print fruit: name, sku only
+        for (int i=0; i<fruitList.size(); i++){
+            if (i == userSku){ // userInput in search for fruit by name FIXME to print all fruit w/ that name
+                System.out.println(i + ": " + fruitList.get[i].getName() + fruitList.get[i].getSku()); //- print fruit: name, sku only
+            }
+        }
+    }
+
+    public void removeFruit(int userInput){ // user input fruit sku
+        if (userInput < 0 || userInput > fruitList.size()){
+            System.out.println("Invalid sku number.");
+        }
+        fruitList.remove(userInput);
+
+    }
+
+}
