75f9aa36da420d858a15731b7023165bb193ab34
mlriley77 <rileycode77@gmail.com>
2017-04-26 09:11:42 -0400

    added fruitinv/standapp/promptuser

--- src/StandApp.java
+++ src/StandApp.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 /**
  * Created by broadwells on 4/25/17.
  */
 public class StandApp {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String fileString = "FruitInventory.txt";
 
+        FruitInventory.CreateFile(fileString);
+        FruitInventory.ReadFile();
+        //FruitInventory.WriteFile(scan);
 
-        System.out.println("Hello World!");
-        System.out.println("Group 1 is awesome");
-        System.out.println("The best ever!");
+        PromptUser.FruitSelection();
+
 
     }
 
