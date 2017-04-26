7b4b1cb342b39dd90afbd885bb6d2699f0cf845c
LydiaLab <lydia@lydialatocki.com>
2017-04-25 23:04:43 -0400

    new Fruit Class

---
+++ src/Fruit.java
@@ -0,0 +1,66 @@
+/**
+ * Created by Pro on 4/25/17.
+ */
+public class Fruit {
+    //instance variables (instead of static variables)
+    private String name;
+    private int sku;
+    private String category;
+    private String descript;
+    private double price;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSku(int sku){
+        return sku;
+    }
+
+    public void setSku(int sku){
+        this.sku = sku;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescript() {
+        return descript;
+    }
+
+    public void setDescript(String descript) {
+        this.descript = descript;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public Fruit(String name, int sku, String category, String descript, double price) {
+        this.name = name;
+        this.sku = sku;
+        this.category = category;
+        this.descript = descript;
+        this.price = price;
+    }
+    @Override
+    public String toString() {
+
+        return name + "\t" + category + "\t" + descript + "\t$" + price;
+    }
+
+}
